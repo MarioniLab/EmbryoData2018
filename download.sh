eae9dd360bd3e227851fdb65f551f9326f280cf3
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2018-06-27 11:02:28 +0100

    Update for broken download script

--- download.sh
+++ download.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
-echo "Beginning ftp"
-wget --user=jmlabftp --password='HOBICAmeer6' ftp://ftp2.cruk.cam.ac.uk/emb/BPSEtAl_data.tar.gz
-echo "You can now unzip the downloaded archive"
+
+echo "Programmatic download is currently broken. Please navigate to \"https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/EmbryoTimecourse2017Data/\" to get the data"
+
+# echo "Beginning ftp"
+# wget --user=jmlabftp --password='HOBICAmeer6' ftp://ftp2.cruk.cam.ac.uk/emb/BPSEtAl_data.tar.gz
+# echo "You can now unzip the downloaded archive"
