b8d28d9fbe31e3966d52102133dd9e41233018b0
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2018-06-27 11:30:25 +0100

    Typo fix

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-echo "Programmatic download is currently broken. Please navigate to \"https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/EmbryoTimecourse2017Data/\" to get the data"
+echo "Programmatic download is currently broken. Please navigate to \"https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/EmbryoTimecourse2018Data/\" to get the data"
 
 # echo "Beginning ftp"
 # wget --user=jmlabftp --password='HOBICAmeer6' ftp://ftp2.cruk.cam.ac.uk/emb/BPSEtAl_data.tar.gz
