ce5ebebaee15b41c3b5cc557b54c4c726f7e9494
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2018-07-05 11:47:45 +0100

    Removed -k from curl

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 echo "Beginning download"
-curl -k https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/EmbryoTimecourse2018Data/raw/master/BPSEtAl_data.tar.gz > BPSEtAl_data.tar.gz
+curl https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/EmbryoTimecourse2018Data/raw/master/BPSEtAl_data.tar.gz > BPSEtAl_data.tar.gz
 echo "You can now unzip the downloaded archive (BPSEtAl_data.tar.gz)"
